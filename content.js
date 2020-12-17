0f3982d5bf906dc96930b31c1ce0b2c3fea83084
Ivar Fredriksen <ivar.fredriksen@origo.oslo.kommune.no>
2020-12-17 12:15:01 +0100

    Initial working plugin

---
+++ content.js
@@ -0,0 +1,38 @@
+// content.js
+
+fieldset = document.getElementsByTagName("fieldset")[0]
+accounts = fieldset.children
+
+signin = document.getElementById("input_signin_button")
+select = document.createElement('select')
+
+for (let account of accounts) {
+    fullAccountName = account.getElementsByClassName("saml-account-name")[0].innerText
+    accountName = fullAccountName.substring(15)
+    radio = account.getElementsByClassName("saml-radio")[0]
+
+    console.log(radio)
+    
+    option = document.createElement('option')
+    
+    option.innerText = accountName
+    option.setAttribute("value", radio.id)
+    
+    select.appendChild(option)
+    select.setAttribute("class", "selector")
+}
+
+fieldset.style.display = "none"
+signin.prepend(select)
+
+$(select).change(function(e) {
+    selectElement = e.srcElement
+    $('.selector').find(':selected').data('value');
+
+    radio = document.getElementById($('.selector').find(':selected').val())
+    radio.checked = true
+})
+
+$(document).ready(function() {
+    $(".selector").select2()
+});
